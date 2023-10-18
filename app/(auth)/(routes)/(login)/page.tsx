20ed990b4057ec8355169b01ce5917c47c8d1b14
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-10-18 19:47:39 +0530

    setting up the environment

---
+++ app/(auth)/(routes)/(login)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      This is the page from login
+    </div>
+  )
+}
+
+export default page
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <p className='text-3xl font-bold text-indigo-500'> This is a protected route </p>
+    </>
+  )
+}
