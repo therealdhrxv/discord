0b84f19491ba37d663f31b97eef1bde8d3380a3b
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-10-21 15:54:30 +0530

    feat: auth setup 🥳

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthLayout = ({ children }: {children: React.ReactNode}) => {
+  return (
+    <div className='flex justify-center h-full items-center'>
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <>
-      <p className='text-3xl font-bold text-indigo-500'> This is a protected route </p>
-    </>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
