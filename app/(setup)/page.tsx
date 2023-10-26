26ccc04ea766e80a3881b54eb3f183a13a28a01d
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-10-26 08:55:35 +0530

    feat: render initial modal to create server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const PageSetup = async () => {
   const profile = await initialProfile();
@@ -19,7 +20,9 @@ const PageSetup = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a Server</div>;
+  return (
+    <InitialModal />
+  ); 
 };
 
 export default PageSetup;
---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+
+import {
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  FormField,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "Server name is required",
+    })
+    .max(255),
+  imageURL: z.string().min(1, {
+    message: "Server image is required",
+  }),
+});
+
+export const InitialModal = () => {
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageURL: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmitMethod = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <Dialog open>
+        <DialogContent className="bg-white text-black p-0 overflow-hidden">
+          <DialogHeader className="pt-8 px-6">
+            <DialogTitle className="text-2xl text-center font-bold">
+              Create your server 🚀
+            </DialogTitle>
+            <DialogDescription className="text-zinc-500 text-center">
+              Give your server a personality by adding a name and an avatar. You
+              can always change these later.
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmitMethod)}
+              className="space-y-8"
+            >
+              <div className="space-y-8 px-6 ">
+                <div className="flex items-center justify-center text-center">
+                  Image upload component here
+                </div>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                        Server name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isLoading}
+                          className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                          placeholder="Enter a server name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter className="bg-gray-100 px-6 py-4">
+                <Button disabled={isLoading} variant="primary">
+                  Create
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
