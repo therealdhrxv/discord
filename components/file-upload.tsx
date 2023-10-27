e59bbe2d8fb30a730d28f037e0cde03eff506cfe
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-10-27 17:08:42 +0530

    fix: show the image and cancel button after upload

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,15 +1,34 @@
 "use client";
 
 import { UploadDropzone } from "@/lib/uploadthing";
+import { X } from "lucide-react";
+import Image from "next/image";
 import "@uploadthing/react/styles.css";
 
 interface FileUploadProps {
   onChange: (url?: String) => void;
-  value: String;
+  value: string;
   endpoint: "serverImage" | "messageFile"; // api/uploadthing/core.ts
 }
 
 export const FileUpload = (props: FileUploadProps) => {
+  const fileType = props.value?.split(".").pop();
+  if (props.value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image src={props.value} alt="Upload" fill className="rounded-full" />
+        <button
+          onClick={() => {
+            props.onChange("");
+          }}
+          className="absolute top-0 right-0 bg-rose-500 text-white shadow-sm rounded-full p-1"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
   return (
     <UploadDropzone
       endpoint="serverImage"
