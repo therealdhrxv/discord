5819937620d6199a083bb93afdf94a8f3bc5a890
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-11-08 01:11:10 +0530

    chore: prettier 🌈

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -4,25 +4,23 @@ import { db } from "@/lib/db";
 import { InitialModal } from "@/components/modals/initial-modal";
 
 const PageSetup = async () => {
-  const profile = await initialProfile();
+    const profile = await initialProfile();
 
-  const server = await db.server.findFirst({
-    where: {
-      members: {
-        some: {
-          profileId: profile.id,
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
         },
-      },
-    },
-  });
+    });
 
-  if (server) {
-    return redirect(`/servers/${server.id}`);
-  }
+    if (server) {
+        return redirect(`/servers/${server.id}`);
+    }
 
-  return (
-    <InitialModal />
-  ); 
+    return <InitialModal />;
 };
 
 export default PageSetup;
--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -4,25 +4,25 @@ import { auth } from "@clerk/nextjs";
 const f = createUploadthing();
 
 const handleAuth = () => {
-  const { userId } = auth();
-  if (!userId) throw new Error("Unauthorized");
-  return { userId: userId };
+    const { userId } = auth();
+    if (!userId) throw new Error("Unauthorized");
+    return { userId: userId };
 };
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  serverImage: f({
-    image: {
-      maxFileSize: "4MB",
-      maxFileCount: 1,
-    },
-  })
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {}),
-  messageFile: f(["image", "pdf"])
-    .middleware(() => handleAuth())
-    .onUploadComplete(() => {}),
+    // Define as many FileRoutes as you like, each with a unique routeSlug
+    serverImage: f({
+        image: {
+            maxFileSize: "4MB",
+            maxFileCount: 1,
+        },
+    })
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => {}),
+    messageFile: f(["image", "pdf"])
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => {}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
