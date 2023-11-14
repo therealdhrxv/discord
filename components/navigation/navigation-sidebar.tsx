728d57c545c0ff101b6ac7c2c10475325af7567a
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-11-14 17:02:02 +0530

    feat: user and theme buttons

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,12 +1,12 @@
+import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { Separator } from "@/components/ui/separator";
-import { NavigationItem } from "@/components/navigation/navigation-item";
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/mode-toggle";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { NavigationAction } from "@/components/navigation/navigation-action";
+import { NavigationItem } from "@/components/navigation/navigation-item";
 import { currentProfile } from "@/lib/current-profile";
-import { redirect } from "next/navigation";
-
-import React from "react";
+import { NavigationAction } from "@/components/navigation/navigation-action";
 
 const NavigationSidebar = async () => {
 	const profile = await currentProfile();
@@ -26,7 +26,7 @@ const NavigationSidebar = async () => {
 	});
 
 	return (
-		<div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3">
+		<div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] bg-[#E3E5E8] py-3">
 			<NavigationAction />
 			<Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
 			<ScrollArea className="flex-1 w-full">
@@ -40,6 +40,17 @@ const NavigationSidebar = async () => {
 					</div>
 				))}
 			</ScrollArea>
+			<div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+				<ModeToggle />
+				<UserButton
+					afterSignOutUrl="/"
+					appearance={{
+						elements: {
+							avatarBox: "h-[48px] w-[48px]",
+						},
+					}}
+				/>
+			</div>
 		</div>
 	);
 };
