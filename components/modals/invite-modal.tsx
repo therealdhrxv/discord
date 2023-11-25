92deb9726e6e4dbb099b782b18ae16b453e433b1
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-11-25 17:48:21 +0530

    feat: invite-friends modal UI setup

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,17 +1,18 @@
 "use client";
+import { Copy, RefreshCw } from "lucide-react";
 
 import { useModal } from "@/hooks/use-modal-store";
-
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import {
 	Dialog,
 	DialogContent,
 	DialogHeader,
 	DialogTitle,
-	DialogDescription,
 } from "@/components/ui/dialog";
 
 export const InviteModal = () => {
-	
 	const { type, isOpen, onClose } = useModal();
 	const isModalOpen = isOpen && type === "invite";
 
@@ -21,15 +22,32 @@ export const InviteModal = () => {
 				<DialogContent className="bg-white text-black p-0 overflow-hidden">
 					<DialogHeader className="pt-8 px-6">
 						<DialogTitle className="text-2xl text-center font-bold">
-							Create your server 🚀
+							Invite Friends
 						</DialogTitle>
-						<DialogDescription className="text-zinc-500 text-center">
-							Give your server a personality by adding a
-							name and an avatar. You can always change
-							these later.
-						</DialogDescription>
 					</DialogHeader>
-					Invite Modal
+					<div className="p-6">
+						<Label className="uppercase text-sx font-bold text-zinc-500 dark:text-secondary/70">
+							{" "}
+							Server invite link{" "}
+						</Label>
+						<div className="flex items-center mt-2 gap-x-2">
+							<Input
+								className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+								value="Invite link"
+							/>
+							<Button size="icon">
+								<Copy className="w-4 h-4" />
+							</Button>
+						</div>
+						<Button
+							variant="link"
+							size="sm"
+							className="text-xs text-zinc-500 mt-4"
+						>
+							Generate a new link
+							<RefreshCw className="w-4 h-4 ml-2" />
+						</Button>
+					</div>
 				</DialogContent>
 			</Dialog>
 		</>
