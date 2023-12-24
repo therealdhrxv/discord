dc5c8740e0177cd5f139bab7c44aa872ec3c2dc7
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-12-24 11:27:03 +0530

    fix: by default render general channel in a server

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,49 @@
-const ServerIdPage = () => {
-    return <div>Server ID Page</div>;
+import { redirect } from "next/navigation";
+import { currentProfile } from "@/lib/current-profile";
+import { redirectToSignIn } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+interface ServerIdPageProps {
+	params: {
+		serverId: string;
+	};
+}
+
+const ServerIdPage = async (props: ServerIdPageProps) => {
+
+	const profile = await currentProfile();
+	if (!profile) return redirectToSignIn();
+
+	const server = await db.server.findUnique({
+		where: {
+			id: props.params.serverId,
+			members: {
+				some: {
+					profileId: profile.id,
+				},
+			},
+		},
+		include: {
+			channels: {
+				where: {
+					name: "general",
+				},
+				orderBy: {
+					createdAt: "asc",
+				},
+			},
+		},
+	});
+
+	const initialChannel = server?.channels[0];
+
+	if (initialChannel?.name !== "general") return null;
+
+	return redirect(
+		`/servers/${props.params.serverId}/channels/${initialChannel?.id}`
+	);
+    
 };
 
 export default ServerIdPage;
