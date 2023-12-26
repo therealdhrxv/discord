815f87724c2827574dd4d1ef07347f65b79fb1c5
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-12-27 01:57:00 +0530

    ui: pdf pre-upload view in upload modal

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { X } from "lucide-react";
+import { X, FileIcon } from "lucide-react";
 import Image from "next/image";
 
 import "@uploadthing/react/styles.css";
@@ -28,6 +28,28 @@ export const FileUpload = (props: FileUploadProps) => {
 						props.onChange("");
 					}}
 					className="absolute top-0 right-0 bg-rose-500 text-white shadow-sm rounded-full p-1"
+					type="button"
+				>
+					<X className="h-4 w-4" />
+				</button>
+			</div>
+		);
+	}
+	if (props.value && fileType === "pdf") {
+		return (
+			<div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+				<FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+				<a
+					href={props.value}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+				>
+					{props.value}
+				</a>
+				<button
+					onClick={() => props.onChange("")}
+					className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
 					type="button"
 				>
 					<X className="h-4 w-4" />
